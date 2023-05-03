df9008466461708611ff508f8ad6f32a697a069e
Nworah-Gabriel <gabrielnworah6@gmail.com>
2023-05-03 13:28:51 +0100

    Added more files

---
+++ cryptobits/settings.py
@@ -0,0 +1,209 @@
+
+
+import os
+import django_heroku
+from django.http import HttpResponseRedirect
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '74f@!wp!as*8v#fy+6c$4k-rhn0pp7w^#9fy#pzi5dgqtuf@bg'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.hodlvault.io', 'hodlvault.io', 'localhost', '127.0.0.1']
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'cryptobitsapp',
+    #  'storages',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+]
+
+ROOT_URLCONF = 'cryptobits.urls'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATE_DIR,],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },  
+    },
+]
+
+WSGI_APPLICATION = 'cryptobits.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = "Africa/Lagos"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+MEDIA_URL = '/media/'
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+    
+# ]
+# STATIC_ROOT =  "/fortecount4.herokuapp.com/static/"
+
+
+# MEDIA_URL = "/media/"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SECURE_HSTS_SECONDS = 31536000
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+X_FRAME_OPTIONS = 'DENY'
+CORS_REPLACE_HTTPS_REFERER      = False
+# HOST_SCHEME                     = "http://"
+# HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = None
+# SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+#gmail
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER='ccryptograminconline@gmail.com'
+
+EMAIL_HOST = 'mail.privateemail.com'
+EMAIL_HOST_USER='support@nft-rarible.net'
+EMAIL_HOST_PASSWORD = 'nft-rarible'
+EMAIL_HOST_TLS='True'
+
+
+
+AWS_ACCESS_KEY_ID = 'AKIATQ643KHOSR2QM4IV'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'hodlvault-crm-bucket'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'media'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'cryptobitsapp/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+#might be changed
+# AWS_S3_FILE_OVERWRITE=False
+AWS_DEFAULT_ACL ='public-read'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = 'admin@hodlvault.io'
+
+
+
+django_heroku.settings(locals())
---
+++ cryptobits/urls.py
@@ -0,0 +1,123 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from cryptobitsapp import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.userlogin, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('updatebal/', views.updatebal, name="updatebal"),
+    # path('user_profile/', views.user_profile, name="user_profile"),
+    path('updateuserbalance/', views.update_balance, name="update_balance"),
+    path('updatefarmbal/', views.updatefarmbal, name="updatefarmbal"),
+    path('update-user-farm-balance/', views.update_user_farm_balance, name="update_user_farm_balance"),
+    path('get-coin-value/', views.get_coin_value, name="get_coin_value"),
+    path('test', views.test, name="test"),
+
+
+
+    path('activate/<uidb64>/<token>', views.VerificationView.as_view(), name = "activate"),
+    path('logout/', views.userlogout, name="logout"),
+    path('detail/', views.detail, name="detail"),
+    path('signup/', views.signup, name="signup"),
+    path('signup-sum/', views.signupsum, name="signup-sum"),
+    path('confirm/', views.confirm, name="confirm"),
+
+
+
+
+   
+
+
+    path('makewithdraw', views.make_withdraw, name="make_withdraw"),
+    path('makedeposit', views.make_deposit, name="make_deposit"),
+    path('create_user_stock', views.create_user_stock, name="create_user_stock"),
+    path('stocksubmit', views.stockSubmit, name="stock_submit"),
+    path('update_userdetail', views.update_userdetail, name="update_userdetail"),
+    path('update_userwithdrawaldetail', views.update_userwithdrawaldetail, name="update_userwithdrawaldetail"),
+
+    path('update', views.update, name="update"),
+    path('investment', views.investment, name="investment"),
+    path('wallet/', views.wallet, name="wallet"),
+    path('wallet_deposit/', views.wallet_deposit, name="wallet_deposit"),
+    path('wallet_withdraw/', views.wallet_withdraw, name="wallet_withdraw"),
+    path('wallet_create/', views.wallet_create, name="wallet_create"),
+    path('withdraw/', views.withdraw, name="withdraw"),
+    path('transactions/', views.transactions, name="transactions"),
+
+
+    path('deposit/', views.deposit, name="deposit"),
+    path('signupextra/', views.signupextra, name="signupextra"),
+    path('farming/', views.farming, name="farming"),
+    path('fund_transfer/', views.fund_transfer, name="fund_transfer"),
+    path('get_stock_defi_value', views.get_stock_defi_value, name="get_stock_defi_value"),
+    path('profile/', views.profile, name="profile"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('account-verification/', views.accountVerification, name="account-verification"),
+    path('verification/', views.verification, name="verification"),
+    path('verify/', views.verify, name="verify"),
+    path('passwordchange', views.password_change, name="passwordchange"),
+    path('update/', views.update, name="update"),
+    path('password/', views.password, name="password"),
+    path('packages/', views.packages, name="packages"),
+    #superuser pages
+
+
+
+    # path('dashboard/', views.dashboard, name="dashboard"),
+    path('test/', views.test, name="test"),
+    # home urls
+    path('', views.index, name=""),
+    path('index', views.index, name=""),
+    path('index', views.index, name="index"),
+    path('location', views.location, name="location"),
+    path('govern', views.govern, name="govern"),
+    path('account', views.account, name="account"),
+    path('product', views.product, name="product"),
+    path('cryptocurrency', views.cryptocurrency, name="cryptocurrency"),
+    path('regulatory', views.regulatory, name="regulatory"),
+    path('real', views.real, name="real"),
+    path('dashboard/real', views.real, name="real"),
+    path('advice', views.advice, name="advice"),
+    path('guidiance', views.guidiance, name="guidiance"),
+    path('wealth', views.wealth, name="wealth"),
+    path('finplan', views.finplan, name="finplan"),
+    path('calculator', views.calculator, name="calculator"),
+    path('complimentary', views.complimentary, name="complimentary"),
+    path('pricing', views.pricing, name="pricing"),
+    path('dashboard/investments', views.investment, name="investments"),
+    path('shares', views.shares, name="shares"),
+    path('dashboard/shares', views.shares, name="shares"),
+    path('stock', views.stock, name="stock"),
+    path('dashboard/stock', views.stock, name="stock"),
+    path('crypto', views.crypto, name="crypto"),
+    path('dashboard/crypto', views.crypto, name="crypto"),
+    path('forex', views.forex, name="forex"),
+    path('dashboard/forex', views.forex, name="forex"),
+    path('realestate', views.restate, name="restate"),
+    path('investmentfee', views.investmentfee, name="investmentfee"),
+    path('whychoose', views.whychoose, name="whychoose"),
+    path('satisfaction', views.satisfaction, name="satisfaction"),
+    path('security', views.security, name="security"),
+    path('ways', views.ways, name="ways"),
+    path('faq', views.faq, name="faq"),
+    path('contact', views.contact, name="contact"),
+    path('support', views.support, name="support"),
+    path('companydetail', views.GetCompanyDetail, name="companydetail"),
+
+    path('about/', views.about, name="about"),
+    
+    path('terms/', views.terms, name="terms"),
+    path('error/', views.error, name="error")
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# handler404='forteapp.views.error_404'
+# handler500 = 'forteapp.views.error_500'
+# handler403='forteapp.views.error_403'
+# handler400='forteapp.views.error_400'
